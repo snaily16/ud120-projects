052d71de9e3cd2c6c4f548728c5158961f7a2387
SNEHAL GHARAT <snaily16@gmail.com>
2018-08-05 01:44:55 +0530

    Update validate_poi.py

--- validation/validate_poi.py
+++ validation/validate_poi.py
@@ -35,10 +35,6 @@ from sklearn.cross_validation import train_test_split
 features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=0.30, random_state=42)
 
 clf = DecisionTreeClassifier()
-clf.fit(features, labels)
-pred = clf.predict(features)
-print accuracy_score(pred, labels)
-"""
 clf.fit(features_train, labels_train)
 pred = clf.predict(features_test)
 print(accuracy_score(labels_test, pred))"""
